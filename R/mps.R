9caa156ebb22cf6fdfdc0ad1f6d3a5c779423b62
Chris Mason-Thom <chrismasonthom@gmail.com>
2025-05-24 10:21:46 +0000

    add MPs dataset

---
+++ R/mps.R
@@ -0,0 +1,8 @@
+
+#' Members of Parliament
+#' 
+#' A data frame of the UK members of parliament, their constituency and party.
+#' 
+#' @source <https://members.parliament.uk/members/commons>
+#' 
+"mps"
--- data-raw/mps.R
+++ data-raw/mps.R
@@ -1,2 +1,23 @@
 
 # prepare MPs dataset
+
+library(readr)
+library(dplyr)
+library(janitor)
+
+mps_raw <- read_csv("data-raw/mps.csv")
+
+mps <- mps_raw |> 
+  clean_names(case = "snake") |> 
+  select(
+    country,
+    pcon_code = constituency_ons,
+    pcon_name = constituency,
+    name,
+    title = addressable_title,
+    party,
+    position,
+    source_url = source_link
+  )
+
+usethis::use_data(mps, overwrite = TRUE)
